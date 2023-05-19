9124a8a83760504351e978651cecc7adafe8e14c
Mathijs Bernson <mathijsb@q42.nl>
2023-05-19 13:37:09 +0200

    Initial Commit

---
+++ TokTik/TokTikApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokTikApp.swift
+//  TokTik
+//
+//  Created by Mathijs Bernson on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TokTikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
